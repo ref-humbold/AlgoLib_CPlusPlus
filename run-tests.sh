ed02d04bf0befb3b125734cbe20beb5b9393eb2b
ref-humbold <rafkaleta@onet.pl>
2017-07-07 00:19:54 +0200

    Kolorowanie wypisywania

--- run-tests.sh
+++ run-tests.sh
@@ -5,17 +5,18 @@ GTEST_OUTPUT_DIR="gtest_results"
 
 if test ! -d $TEST_DIR
 then
-    echo "Test directory not found."
+    echo "\033[0;31mTest directory not found.\033[0m"
     exit -1
 fi
 
 rm -rf $GTEST_OUTPUT_DIR && mkdir $GTEST_OUTPUT_DIR
-echo "Running GTest tests:"
+echo "\033[1;35mRunning GTest tests:\033[0m"
 for FILE in $TEST_DIR/*
 do
     if test -x $FILE
     then
-        echo "    >>>> $FILE <<<<"
+        echo ""
+        echo "\033[1;35m    >>>> $FILE <<<<\033[0m"
         ./$FILE --gtest_output="xml:./$GTEST_OUTPUT_DIR/"
     fi
 done
