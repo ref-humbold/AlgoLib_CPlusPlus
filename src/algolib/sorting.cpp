3b98ba8f150eae9a51a93d81936ad81312e1ab71
ref-humbold <rafkaleta@onet.pl>
2017-07-07 14:54:36 +0200

    Testy i refaktoryzacja sortowania

--- src/algolib/sorting.cpp
+++ src/algolib/sorting.cpp
@@ -1,2 +1,44 @@
 // ALGORYTMY SORTOWANIA
 #include "sorting.hpp"
+
+int detail::choose_pivot(int size)
+{
+    srand(time(0));
+
+    int candidate1 = rand() % size, candidate2 = rand() % size, candidate3 = rand() % size;
+
+    if(std::min(candidate2, candidate3) <= candidate1
+       && candidate1 <= std::max(candidate2, candidate3))
+        return candidate2;
+    else if(std::min(candidate1, candidate3) <= candidate2
+            && candidate2 <= std::max(candidate1, candidate3))
+        return candidate2;
+    else
+        return candidate3;
+}
+
+void algolib::angle_sort(std::vector<point2D_t> & points)
+{
+    auto angle = [](const point2D_t & pt) {
+        double ang = atan2(pt.second, pt.first) * 180.0 / M_PI;
+
+        return pt.second >= 0.0 ? ang : ang + 360.0;
+    };
+
+    auto radius = [](const point2D_t & pt) { return pt.first * pt.first + pt.second * pt.second; };
+
+    auto comparator = [&](const point2D_t & pt1, const point2D_t & pt2) -> bool {
+        double angle1 = angle(pt1), angle2 = angle(pt2);
+
+        return angle1 != angle2 ? angle1 < angle2 : radius(pt1) <= radius(pt2);
+    };
+
+    std::sort(points.begin(), points.end(), comparator);
+}
+
+std::vector<point2D_t> algolib::angle_sorted(std::vector<point2D_t> points)
+{
+    angle_sort(points);
+
+    return points;
+}
--- src/algolib/sorting.hpp
+++ src/algolib/sorting.hpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <cmath>
 #include <ctime>
+#include <exception>
+#include <stdexcept>
 #include <algorithm>
 #include <vector>
 
@@ -23,20 +25,20 @@ namespace detail
     void move_down(std::vector<T> & heap, int vertex, int index_begin, int index_end)
     {
         int next_vertex = -1;
-        int left_vertex = (vertex * 2) - index_begin + 1;
-        int right_vertex = (vertex * 2) - index_begin + 2;
+        int left_vertex = vertex + vertex - index_begin + 1;
+        int right_vertex = vertex + vertex - index_begin + 2;
 
-        if(right_vertex <= index_end)
+        if(right_vertex < index_end)
             next_vertex = heap[right_vertex] < heap[left_vertex] ? left_vertex : right_vertex;
 
-        if(left_vertex == index_end)
+        if(left_vertex == index_end - 1)
             next_vertex = left_vertex;
 
         if(next_vertex < 0)
             return;
 
         if(heap[next_vertex] > heap[vertex])
-            swap(heap[next_vertex], heap[vertex]);
+            std::swap(heap[next_vertex], heap[vertex]);
 
         move_down(heap, next_vertex, index_begin, index_end);
     }
@@ -52,9 +54,9 @@ namespace detail
     void merge(std::vector<T> & sequence, int index_begin, int index_middle, int index_end)
     {
         std::vector<T> ordered;
-        int iter1 = index_begin, iter2 = index_middle + 1;
+        int iter1 = index_begin, iter2 = index_middle;
 
-        while(iter1 <= index_middle && iter2 <= index_end)
+        while(iter1 < index_middle && iter2 < index_end)
             if(sequence[iter1] < sequence[iter2])
             {
                 ordered.push_back(sequence[iter1]);
@@ -66,13 +68,11 @@ namespace detail
                 ++iter2;
             }
 
-        if(iter1 <= index_middle)
-            for(int i = iter1; i <= index_middle; ++i)
-                ordered.push_back(sequence[i]);
+        for(int i = iter1; i < index_middle; ++i)
+            ordered.push_back(sequence[i]);
 
-        if(iter2 <= index_end)
-            for(int i = iter2; i <= index_end; ++i)
-                ordered.push_back(sequence[i]);
+        for(int i = iter2; i < index_end; ++i)
+            ordered.push_back(sequence[i]);
 
         for(int i = 0; i < ordered.size(); ++i)
             sequence[index_begin + i] = ordered[i];
@@ -83,21 +83,7 @@ namespace detail
      * @param size liczba elementów
      * @return indeks piwota
      */
-    int choose_pivot(int size)
-    {
-        srand(time(0));
-
-        int candidate1 = rand() % size, candidate2 = rand() % size, candidate3 = rand() % size;
-
-        if(std::min(candidate2, candidate3) <= candidate1
-           && candidate1 <= std::max(candidate2, candidate3))
-            return candidate2;
-        else if(std::min(candidate1, candidate3) <= candidate2
-                && candidate2 <= std::max(candidate1, candidate3))
-            return candidate2;
-        else
-            return candidate3;
-    }
+    int choose_pivot(int size);
 }
 
 namespace algolib
@@ -106,38 +92,14 @@ namespace algolib
      * Mutowalne sortowanie kątowe punktów na płaszczyźnie.
      * @param points lista punktów
      */
-    void angle_sort(std::vector<point2D_t> & points)
-    {
-        auto angle = [](const point2D_t & pt) {
-            double ang = atan2(pt.second, pt.first) * 180.0 / M_PI;
-
-            return pt.second >= 0.0 ? ang : ang + 360.0;
-        };
-
-        auto radius = [](const point2D_t & pt) {
-            return pt.first * pt.first + pt.second * pt.second;
-        };
-
-        auto comparator = [&](const point2D_t & pt1, const point2D_t & pt2) -> bool {
-            double angle1 = angle(pt1), angle2 = angle(pt2);
-
-            return angle1 != angle2 ? angle1 < angle2 : radius(pt1) <= radius(pt2);
-        };
-
-        std::sort(points.begin(), points.end(), comparator);
-    }
+    void angle_sort(std::vector<point2D_t> & points);
 
     /**
      * Niemutowalne sortowanie kątowe punktów na płaszczyźnie.
      * @param points lista punktów
      * @return lista punktów posortowana względem kąta
      */
-    std::vector<point2D_t> angle_sorted(std::vector<point2D_t> points)
-    {
-        angle_sort(points);
-
-        return points;
-    }
+    std::vector<point2D_t> angle_sorted(std::vector<point2D_t> points);
 
     /**
      * Mutowalne sortowanie ciągu przez kopcowanie.
@@ -148,10 +110,19 @@ namespace algolib
     template <typename T>
     void heap_sort(std::vector<T> & sequence, int index_begin = 0, int index_end = -1)
     {
-        index_begin = (index_begin + sequence.size()) % sequence.size();
-        index_end = (index_end + sequence.size()) % sequence.size();
+        if(index_end == -1)
+            index_end = sequence.size();
+
+        if(index_begin < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence beginning index out of range.");
+
+        if(index_end < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence ending index out of range.");
 
         int heap_size = index_end - index_begin;
+
+        if(heap_size <= 1)
+            return;
 
         for(int i = index_begin + heap_size / 2; i >= index_begin; --i)
             detail::move_down(sequence, i, index_begin, index_end);
@@ -161,7 +132,8 @@ namespace algolib
             int index_heap = index_begin + heap_size;
 
             std::swap(sequence[index_heap], sequence[index_begin]);
-            detail::move_down(sequence, index_begin, index_begin, index_heap - 1);
+            detail::move_down(sequence, index_begin, index_begin, index_heap);
+            --heap_size;
         }
     }
 
@@ -170,6 +142,7 @@ namespace algolib
      * @param sequence ciąg
      * @param index_begin początkowy indeks ciągu
      * @param index_end końcowy indeks ciągu
+     * @return ciąg posortowanych elementów
      */
     template <typename T>
     std::vector<T> heap_sorted(std::vector<T> sequence, int index_begin = 0, int index_end = -1)
@@ -188,16 +161,22 @@ namespace algolib
     template <typename T>
     void merge_sort(std::vector<T> & sequence, int index_begin = 0, int index_end = -1)
     {
-        index_begin = (index_begin + sequence.size()) % sequence.size();
-        index_end = (index_end + sequence.size()) % sequence.size();
+        if(index_end == -1)
+            index_end = sequence.size();
 
-        if(index_begin >= index_end)
+        if(index_begin < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence beginning index out of range.");
+
+        if(index_end < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence ending index out of range.");
+
+        if(index_end - index_begin <= 1)
             return;
 
         int index_middle = (index_begin + index_end) / 2;
 
         merge_sort(sequence, index_begin, index_middle);
-        merge_sort(sequence, index_middle + 1, index_end);
+        merge_sort(sequence, index_middle, index_end);
         detail::merge(sequence, index_begin, index_middle, index_end);
     }
 
@@ -206,7 +185,7 @@ namespace algolib
      * @param sequence ciąg
      * @param index_begin początkowy indeks ciągu
      * @param index_end końcowy indeks ciągu
-     * @return posortowany ciąg
+     * @return ciąg posortowanych elementów
      */
     template <typename T>
     std::vector<T> merge_sorted(std::vector<T> sequence, int index_begin = 0, int index_end = -1)
@@ -225,14 +204,20 @@ namespace algolib
     template <typename T>
     void quick_sort(std::vector<T> & sequence, int index_begin = 0, int index_end = -1)
     {
-        index_begin = (index_begin + sequence.size()) % sequence.size();
-        index_end = (index_end + sequence.size()) % sequence.size();
+        if(index_end == -1)
+            index_end = sequence.size();
 
-        if(index_begin >= index_end)
+        if(index_begin < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence beginning index out of range.");
+
+        if(index_end < 0 || index_end > sequence.size())
+            throw std::invalid_argument("Sequence ending index out of range.");
+
+        if(index_end - index_begin <= 1)
             return;
 
-        int index_pivot = index_begin, index_front = index_begin + 1, index_back = index_end;
-        int rdpv = index_begin + detail::choose_pivot(index_end - index_begin + 1);
+        int index_pivot = index_begin, index_front = index_begin + 1, index_back = index_end - 1;
+        int rdpv = index_begin + detail::choose_pivot(index_end - index_begin);
 
         std::swap(sequence[index_pivot], sequence[rdpv]);
 
@@ -249,7 +234,7 @@ namespace algolib
                 --index_back;
             }
 
-        quick_sort(sequence, index_begin, index_pivot - 1);
+        quick_sort(sequence, index_begin, index_pivot);
         quick_sort(sequence, index_pivot + 1, index_end);
     }
 
@@ -258,7 +243,7 @@ namespace algolib
      * @param sequence ciąg
      * @param index_begin początkowy indeks ciągu
      * @param index_end końcowy indeks ciągu
-     * @return posortowany ciąg
+     * @return ciąg posortowanych elementów
      */
     template <typename T>
     std::vector<T> quick_sorted(std::vector<T> sequence, int index_begin = 0, int index_end = -1)
