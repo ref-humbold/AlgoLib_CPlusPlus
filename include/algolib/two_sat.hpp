15dfe306c23789584811bae2e4f2ff468645b0e8
R. Kaleta <rafkaleta@onet.pl>
2019-05-09 01:43:56 +0200

    Removed 2sat

--- include/algolib/two_sat.hpp
+++
@@ -1,9 +0,0 @@
-// 2-SATISFACTORY ALGORITHM
-#ifndef _TWO_SAT_HPP_
-#define _TWO_SAT_HPP_
-
-namespace algolib
-{
-}
-
-#endif
--- src/algolib/two_sat.cpp
+++
@@ -1,2 +0,0 @@
-// 2-SATISFACTORY ALGORITHM
-#include "algolib/two_sat.hpp"
