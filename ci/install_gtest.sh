08a7ed9e9d1163870506800742a5deaf93e79ecd
R. Kaleta <rafkaleta@onet.pl>
2020-02-01 23:42:32 +0100

    Fix copying

--- ci/install_gtest.sh
+++ ci/install_gtest.sh
@@ -6,7 +6,6 @@ sudo -i -- sh -c \
 "cd /usr/src/googletest/googletest && \
 mkdir build && \
 cd build && \
-pwd && \
 cmake .. && \
 make && \
-cp /usr/src/googletest/googletest/build/lib/*.a /usr/local/lib/"
+cp *.a /usr/local/lib/"
