2fd4427a6418871a259b16e32def33b3003629f3
R. Kaleta <rafkaleta@onet.pl>
2020-02-01 23:18:49 +0100

    Fixed copying googletest libs

--- ci/install_gtest.sh
+++ ci/install_gtest.sh
@@ -3,9 +3,10 @@
 set -e
 
 sudo -i \
-cd /usr/src/googletest/googletest && \
-mkdir build && \
-cd build && \
-cmake .. && \
-make && \
-cp *.a /usr/local/lib/
+  cd /usr/src/googletest/googletest && \
+  mkdir build && \
+  cd build && \
+  cmake .. && \
+  make
+
+cp /usr/src/googletest/googletest/lib/*.a /usr/local/lib/
