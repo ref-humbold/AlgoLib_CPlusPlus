70a36929dc23740d369985065ee79e9df8e8c2b7
R. Kaleta <rafkaleta@onet.pl>
2020-02-01 23:28:14 +0100

    Check directory

--- ci/install_gtest.sh
+++ ci/install_gtest.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-set -e
+set -ex
 
 sudo -i \
     cd /usr/src/googletest/googletest && \
     mkdir build && \
-    cd build  && \
+    cd build && \
+    pwd && \
     cmake .. && \
     make && \
     cp /usr/src/googletest/googletest/build/lib/*.a /usr/local/lib/
