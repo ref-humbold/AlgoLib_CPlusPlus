47014a29a6d8e77187b6e98592f28afebfa34a94
R. Kaleta <rafkaleta@onet.pl>
2020-02-01 23:39:26 +0100

    Run script with sudo

--- ci/install_gtest.sh
+++ ci/install_gtest.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-set -ex
+set -e
 
-sudo -i \
-    cd /usr/src/googletest/googletest && \
-    mkdir build && \
-    cd build && \
-    pwd && \
-    cmake .. && \
-    make && \
-    cp /usr/src/googletest/googletest/build/lib/*.a /usr/local/lib/
+sudo -i -- sh -c \
+"cd /usr/src/googletest/googletest && \
+mkdir build && \
+cd build && \
+pwd && \
+cmake .. && \
+make && \
+cp /usr/src/googletest/googletest/build/lib/*.a /usr/local/lib/"
