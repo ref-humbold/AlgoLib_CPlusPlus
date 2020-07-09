1d6d7b394324dfec165a5dbc7b2810ab29909df9
R. Kaleta <rafkaleta@onet.pl>
2020-07-09 22:31:25 +0200

    Lowest common ancestor

---
+++ include/algolib/graphs/algorithms/lowest_common_ancestor.hpp
@@ -0,0 +1,168 @@
+/*!
+ * \file lowest_common_ancestor.hpp
+ * \brief Algorithm for lowest common ancestor
+ */
+#ifndef LOWEST_COMMON_ANCESTOR_HPP_
+#define LOWEST_COMMON_ANCESTOR_HPP_
+
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+#include "algolib/graphs/algorithms/searching.hpp"
+#include "algolib/graphs/tree_graph.hpp"
+
+namespace algolib
+{
+    namespace graphs
+    {
+#pragma region lowest_common_ancestor
+
+        template <typename V = size_t, typename VP = no_prop, typename EP = no_prop>
+        class lowest_common_ancestor
+        {
+        public:
+            using vertex_type = typename tree_graph<V, VP, EP>::vertex_type;
+
+        private:
+            class lca_strategy;
+
+        public:
+            lowest_common_ancestor(const tree_graph<V, VP, EP> & graph, const vertex_type & root)
+                : graph{graph}, root_{root}, empty{true}
+            {
+            }
+
+            const vertex_type & root()
+            {
+                return this->root_;
+            }
+
+            /*!
+             * \brief Finds a lowest common ancestor of two vertices in a rooted tree.
+             * \param vertex1 first vertex
+             * \param vertex2 second vertex
+             * \return lowest common ancestor of given vertices
+             */
+            int find(const vertex_type & vertex1, const vertex_type & vertex2)
+            {
+                if(this->empty)
+                    this->initialize();
+
+                return this->do_find(vertex1, vertex2);
+            }
+
+            const tree_graph<V, VP, EP> & graph;
+
+        private:
+            void initialize();
+            int do_find(const vertex_type & vertex1, const vertex_type & vertex2);
+            bool is_offspring(const vertex_type & vertex1, const vertex_type & vertex2);
+
+            vertex_type root_;
+            bool empty;
+            std::unordered_map<vertex_type, std::vector<vertex_type>> paths;
+            lca_strategy strategy;
+        };
+
+        template <typename V, typename VP, typename EP>
+        int lowest_common_ancestor<V, VP, EP>::do_find(
+                const lowest_common_ancestor<V, VP, EP>::vertex_type & vertex1,
+                const lowest_common_ancestor<V, VP, EP>::vertex_type & vertex2)
+        {
+            if(this->is_offspring(vertex1, vertex2))
+                return vertex2;
+
+            if(this->is_offspring(vertex2, vertex1))
+                return vertex1;
+
+            std::vector<vertex_type> candidates = paths[vertex1];
+
+            auto it = std::find_if(candidates.rbegin(), candidates.rend(),
+                                   [&](const vertex_type & candidate) {
+                                       return !this->is_offspring(vertex2, candidate);
+                                   });
+
+            return it != candidates.rend() ? do_find(*it, vertex2)
+                                           : do_find(paths[vertex1][0], vertex2);
+        }
+
+        template <typename V, typename VP, typename EP>
+        void lowest_common_ancestor<V, VP, EP>::initialize()
+        {
+            dfs_recursive(this->graph, this->strategy, {this->root_});
+
+            for(auto && vertex : this->graph.vertices())
+                this->paths.emplace(vertex,
+                                    std::vector<vertex_type>({this->strategy.parents[vertex]}));
+
+            for(int i = 0; i < std::log2(this->graph.vertices_count()) + 3; ++i)
+                for(auto && vertex : this->graph.vertices())
+                    this->paths[vertex].push_back(this->paths[this->paths[vertex][i]][i]);
+
+            this->empty = false;
+        }
+
+        template <typename V, typename VP, typename EP>
+        bool lowest_common_ancestor<V, VP, EP>::is_offspring(
+                const typename lowest_common_ancestor<V, VP, EP>::vertex_type & vertex1,
+                const typename lowest_common_ancestor<V, VP, EP>::vertex_type & vertex2)
+        {
+            return this->strategy.pre_times[vertex1] >= this->strategy.pre_times[vertex2]
+                   && this->strategy.post_times[vertex1] <= this->strategy.post_times[vertex2];
+        }
+
+#pragma endregion
+#pragma region lca_strategy
+
+        template <typename V, typename VP, typename EP>
+        class lowest_common_ancestor<V, VP, EP>::lca_strategy
+            : public dfs_strategy<typename lowest_common_ancestor<V, VP, EP>::vertex_type>
+        {
+        public:
+            using vertex_type = typename lowest_common_ancestor<V, VP, EP>::vertex_type;
+
+            lca_strategy() : timer{0}
+            {
+            }
+
+            void for_root(const vertex_type & root) override
+            {
+                this->parents.emplace(root, root);
+            }
+
+            void on_entry(const vertex_type & vertex) override
+            {
+                this->pre_times.emplace(vertex, this->timer);
+                ++this->timer;
+            }
+
+            void on_next_vertex(const vertex_type & vertex, const vertex_type & neighbour) override
+            {
+                this->parents.emplace(neighbour, vertex);
+            }
+
+            void on_exit(const vertex_type & vertex) override
+            {
+                this->post_times.emplace(vertex, this->timer);
+                ++this->timer;
+            }
+
+            void on_edge_to_visited(const vertex_type &, const V &) override
+            {
+            }
+
+            std::unordered_map<vertex_type, vertex_type> parents;
+            std::unordered_map<vertex_type, int> pre_times;
+            std::unordered_map<vertex_type, int> post_times;
+
+        private:
+            int timer;
+        };
+
+#pragma endregion
+    }
+}
+
+#endif
--- test/lowest_common_ancestor_test.cpp
+++ test/lowest_common_ancestor_test.cpp
@@ -1,85 +1,89 @@
-//! TESTY DLA ALGORYTMU NAJNIŻSZEGO WSPÓLNEGO PRZODKA
+/*!
+ * \file lowest_common_ancestor_test.cpp
+ * \brief Tests: Algorithm for lowest common ancestor
+ */
 #include <gtest/gtest.h>
-#include "algolib/old_graphs/algorithms/lowest_common_ancestor.hpp"
+#include "algolib/graphs/algorithms/lowest_common_ancestor.hpp"
 
 namespace algr = algolib::graphs;
 
-class LCATest : public ::testing::Test
+class LowestCommonAncestorTest : public ::testing::Test
 {
+public:
+    using vertex_t = algr::lowest_common_ancestor<>::vertex_type;
+
 protected:
-    algr::tree_graph trees;
+    algr::tree_graph<> tree;
+    algr::lowest_common_ancestor<> test_object;
 
 public:
-    LCATest()
-        : trees{algr::tree_graph(
-                10, std::vector<edge_t>(
-                            {std::make_pair(0, 1), std::make_pair(0, 2), std::make_pair(1, 3),
-                             std::make_pair(1, 4), std::make_pair(1, 5), std::make_pair(2, 6),
-                             std::make_pair(4, 7), std::make_pair(6, 8), std::make_pair(6, 9)}))}
+    LowestCommonAncestorTest()
+        : tree{algr::tree_graph<>(0)}, test_object{algr::lowest_common_ancestor<>(tree, 0)}
     {
+        tree.add_vertex(1, 0);
+        tree.add_vertex(2, 0);
+        tree.add_vertex(3, 1);
+        tree.add_vertex(4, 1);
+        tree.add_vertex(5, 1);
+        tree.add_vertex(6, 2);
+        tree.add_vertex(7, 4);
+        tree.add_vertex(8, 6);
+        tree.add_vertex(9, 6);
     }
 
-    ~LCATest() override = default;
+    ~LowestCommonAncestorTest() override = default;
 };
 
-TEST_F(LCATest, findLCA_whenSameVertex)
+TEST_F(LowestCommonAncestorTest, find_whenSameVertex_thenVertexIsLCA)
 {
+    // given
     vertex_t vertex = 6;
-
-    vertex_t result = algr::find_lca(trees, vertex, vertex);
-
+    // when
+    vertex_t result = test_object.find(vertex, vertex);
+    // then
     EXPECT_EQ(vertex, result);
 }
 
-TEST_F(LCATest, findLCA_whenVerticesChanged)
+TEST_F(LowestCommonAncestorTest, find_whenVerticesInDifferentSubtrees_thenLCA)
 {
-    vertex_t vertex1 = 5;
-    vertex_t vertex2 = 7;
-
-    vertex_t result1 = algr::find_lca(trees, vertex1, vertex2);
-    vertex_t result2 = algr::find_lca(trees, vertex2, vertex1);
-
-    EXPECT_EQ(1, result1);
-    EXPECT_EQ(1, result2);
-}
-
-TEST_F(LCATest, findLCA_whenVerticesInDifferentSubtrees)
-{
-    vertex_t vertex1 = 5;
-    vertex_t vertex2 = 7;
-
-    vertex_t result = algr::find_lca(trees, vertex1, vertex2);
-
+    // when
+    vertex_t result = test_object.find(5, 7);
+    // then
     EXPECT_EQ(1, result);
 }
 
-TEST_F(LCATest, findLCA_whenRootIsLCA)
+TEST_F(LowestCommonAncestorTest, find_whenVerticesSwapped_thenSameLCA)
 {
-    vertex_t vertex1 = 3;
-    vertex_t vertex2 = 9;
-    vertex_t root = 0;
-
-    vertex_t result = algr::find_lca(trees, vertex1, vertex2, root);
-
-    EXPECT_EQ(root, result);
+    // when
+    vertex_t result1 = test_object.find(5, 7);
+    vertex_t result2 = test_object.find(7, 5);
+    // then
+    EXPECT_EQ(1, result1);
+    EXPECT_EQ(result1, result2);
 }
 
-TEST_F(LCATest, findLCA_whenVerticesAreOffsprings)
+TEST_F(LowestCommonAncestorTest, find_whenRootIsCommonAncestor_thenRoot)
 {
-    vertex_t vertex1 = 8;
-    vertex_t vertex2 = 2;
+    // when
+    vertex_t result = test_object.find(3, 9);
+    // then
+    EXPECT_EQ(test_object.root(), result);
+}
 
-    vertex_t result = algr::find_lca(trees, vertex1, vertex2);
-
+TEST_F(LowestCommonAncestorTest, find_whenVerticesAreOnSamePathFromRoot_thenLCAIsCloserToRoot)
+{
+    // given
+    vertex_t vertex1 = 8, vertex2 = 2;
+    // when
+    vertex_t result = test_object.find(vertex1, vertex2);
+    // then
     EXPECT_EQ(vertex2, result);
 }
 
-TEST_F(LCATest, findLCA_whenRootIsOneOfVertices)
+TEST_F(LowestCommonAncestorTest, find_whenRootIsOneOfVertices_thenRoot)
 {
-    vertex_t vertex1 = 4;
-    vertex_t vertex2 = 0;
-
-    vertex_t result = algr::find_lca(trees, vertex1, vertex2, vertex2);
-
-    EXPECT_EQ(vertex2, result);
+    // when
+    vertex_t result = test_object.find(4, test_object.root());
+    // then
+    EXPECT_EQ(test_object.root(), result);
 }
