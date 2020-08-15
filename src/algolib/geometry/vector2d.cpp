bb81f6794b21acfb307a2ec9d151e848d6853f78
R. Kaleta <rafkaleta@onet.pl>
2020-08-15 21:24:52 +0200

    Update vector2d.cpp

--- src/algolib/geometry/vector2d.cpp
+++ src/algolib/geometry/vector2d.cpp
@@ -56,13 +56,6 @@ alge::vector2d alge::operator/(alge::vector2d v, double c)
     return v;
 }
 
-alge::vector2d alge::operator/(double c, alge::vector2d v)
-{
-    v.x_ /= c;
-    v.y_ /= c;
-    return v;
-}
-
 std::ostream & alge::operator<<(std::ostream & os, const alge::vector2d & v)
 {
     os << "[" << v.x_ << ", " << v.y_ << "]";
