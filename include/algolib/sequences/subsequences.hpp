d480e71f91a7293302206d3760f8e3caa0b3d73e
R. Kaleta <rafkaleta@onet.pl>
2021-08-27 00:53:01 +0200

    Tests for subsequences

--- include/algolib/sequences/subsequences.hpp
+++ include/algolib/sequences/subsequences.hpp
@@ -7,55 +7,53 @@
 
 #include <cstdlib>
 #include <algorithm>
-#include <vector>
 #include <functional>
+#include <vector>
 
 namespace internal
 {
-    template <typename T>
-    int search_ord(const std::function<bool(T, T)> & order, const std::vector<T> & sequence,
-                   const std::vector<int> & subseq_last, int index_begin, int index_end,
-                   int index_elem)
+    template <typename T, typename C = std::less<T>>
+    int search_index(const std::vector<T> & sequence, const C & compare,
+                     const std::vector<int> & subseq_last, int index_begin, int index_end,
+                     int index_elem)
     {
         if(index_begin == index_end)
             return index_begin;
 
         int index_middle = (index_begin + index_end) / 2;
 
-        if(order(sequence[index_elem], sequence[subseq_last[index_middle]]))
-            return search_ord(order, sequence, subseq_last, index_middle + 1, index_end,
-                              index_elem);
+        if(compare(sequence[subseq_last[index_middle]], sequence[index_elem]))
+            return search_index(sequence, compare, subseq_last, index_middle + 1, index_end,
+                                index_elem);
 
-        return search_ord(order, sequence, subseq_last, index_begin, index_middle, index_elem);
+        return search_index(sequence, compare, subseq_last, index_begin, index_middle, index_elem);
     }
 }
 
 namespace algolib::sequences
 {
     /*!
-     * \brief Constructs longest ordered subsequence
-     * \param sequence sequence of elements
-     * \param order order function of elements in subsequence
-     * \return longest ordered subsequence (least lexicographically)
+     * \brief Constructs the longest increasing subsequence according to given order function.
+     * \param sequence a sequence of elements
+     * \return the longest increasing subsequence (least lexicographically)
      */
-    template <typename T>
-    std::vector<T> longest_ordered(const std::vector<T> & sequence,
-                                   const std::function<bool(T, T)> & order)
+    template <typename T, typename C = std::less<T>>
+    std::vector<T> longest_increasing(const std::vector<T> & sequence, const C & compare = C())
     {
         std::vector<T> longest_subseq;
         std::vector<int> subseq_last(1, 0);
         std::vector<int> previous_elems(sequence.size(), -1);
 
         for(size_t i = 1; i < sequence.size(); ++i)
-            if(order(sequence[i], sequence[subseq_last.back()]))
+            if(compare(sequence[subseq_last.back()], sequence[i]))
             {
                 previous_elems[i] = subseq_last.back();
                 subseq_last.push_back(i);
             }
             else
             {
-                int index = internal::search_ord(order, sequence, subseq_last, 0,
-                                                 subseq_last.size() - 1, i);
+                int index = internal::search_index<T>(sequence, compare, subseq_last, 0,
+                                                      subseq_last.size() - 1, i);
 
                 subseq_last[index] = i;
                 previous_elems[i] = index > 0 ? subseq_last[index - 1] : -1;
@@ -64,22 +62,21 @@ namespace algolib::sequences
         for(int j = subseq_last.back(); j >= 0; j = previous_elems[j])
             longest_subseq.push_back(sequence[j]);
 
-        reverse(longest_subseq.begin(), longest_subseq.end());
-
+        std::reverse(longest_subseq.begin(), longest_subseq.end());
         return longest_subseq;
     }
 
     /*!
-     * \brief Wyznaczanie spójnego podciągu o maksymalnej sumie w sposób dynamiczny
-     * \param sequence ciąg
-     * \return elementy spójnego podciągu o maksymalnej sumie
+     * \brief Dynamically constructs coherent subarray with maximal sum.
+     * \param sequence a sequence of numbers
+     * \return the maximum subarray
      */
     std::vector<double> maximum_subarray(const std::vector<double> & sequence);
 
     /*!
-     * \brief Wyznaczanie maksymalnej sumy spójnego podciągu za pomocą drzewa przedziałowego
-     * \param sequence ciąg
-     * \return maksymalna suma
+     * \brief Calculates maximal sum from all coherent subarrays using interval tree.
+     * \param sequence a sequence of numbers
+     * \return the sum of maximum subarray
      */
     double maximal_subsum(const std::vector<double> & sequence);
 }
--- test/sequences/subsequences_test.cpp
+++ test/sequences/subsequences_test.cpp
@@ -7,56 +7,122 @@
 
 namespace alse = algolib::sequences;
 
-TEST(MaximumSubarrayTest, maximumSubarray1)
+#pragma region longest_increasing
+
+TEST(SubsequencesTest, longestIncreasing_WhenIncreasing_ThenAllElements)
 {
+    // given
+    std::vector<int> sequence = {1, 3, 5, 7, 9, 11, 13, 15};
+    // when
+    std::vector<int> result = alse::longest_increasing(sequence);
+    // then
+    EXPECT_EQ(sequence, result);
+}
+
+TEST(SubsequencesTest, longestIncreasing_WhenDecreasing_ThenLastElementOnly)
+{
+    // given
+    std::vector<int> sequence = {12, 10, 8, 6, 4, 2};
+    // when
+    std::vector<int> result = alse::longest_increasing(sequence);
+    // then
+    EXPECT_EQ(std::vector<int>({2}), result);
+}
+
+TEST(SubsequencesTest, longestIncreasing_WhenMultipleSubsequences_ThenLeastLexicographically)
+{
+    // given
+    std::vector<int> sequence = {2, 1, 4, 3, 6, 5, 8, 7, 10};
+    // when
+    std::vector<int> result = alse::longest_increasing(sequence);
+    // then
+    EXPECT_EQ(std::vector<int>({1, 3, 5, 7, 10}), result);
+}
+
+TEST(SubsequencesTest, longestIncreasing_WhenIncreasingAndReversedComparator_ThenLastElementOnly)
+{
+    // given
+    std::vector<int> sequence = {1, 3, 5, 7, 9, 11, 13, 15};
+    // when
+    std::vector<int> result = alse::longest_increasing<int, std::greater<int>>(sequence);
+    // then
+    EXPECT_EQ(std::vector<int>({15}), result);
+}
+
+TEST(SubsequencesTest, longestIncreasing_WhenDecreasingAndReversedComparator_ThenAllElements)
+{
+    // given
+    std::vector<int> sequence = {12, 10, 8, 6, 4, 2};
+    // when
+    std::vector<int> result = alse::longest_increasing<int, std::greater<int>>(sequence);
+    // then
+    EXPECT_EQ(sequence, result);
+}
+
+#pragma endregion
+#pragma region maximum_subarray
+
+TEST(SubsequencesTest, maximumSubarray_WhenNegativeIsLessThanSubsum_ThenIncludesNegative)
+{
+    // given
     std::vector<double> sequence = {3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8};
-
+    // when
     std::vector<double> result = alse::maximum_subarray(sequence);
-
+    // then
     EXPECT_EQ(std::vector<double>({3.5, 4.8, -1.6, 7.7, 2.1}), result);
 }
 
-TEST(MaximumSubarrayTest, maximumSubarray2)
+TEST(SubsequencesTest, maximumSubarray_WhenNegativeIsGreaterThanSubsum_ThenExcludesNegative)
 {
+    // given
     std::vector<double> sequence = {-9.3, -1.2, 3.5, 4.8, -10.6, 7.7, 2.1, 0.8, 4.0};
-
+    // when
     std::vector<double> result = alse::maximum_subarray(sequence);
-
+    // then
     EXPECT_EQ(std::vector<double>({7.7, 2.1, 0.8, 4.0}), result);
 }
 
-TEST(MaximumSubarrayTest, maximumSubarray_WhenAllElementsAreNegative)
+TEST(SubsequencesTest, maximumSubarray_WhenAllElementsAreNegative_ThenEmpty)
 {
+    // given
     std::vector<double> sequence = {-9.0, -2.4, -3.07, -1.93, -12.67};
-
+    // when
     std::vector<double> result = alse::maximum_subarray(sequence);
-
+    // then
     EXPECT_EQ(std::vector<double>(), result);
 }
 
-TEST(MaximumSubarrayTest, maximalSubsum1)
+#pragma endregion
+#pragma region maximal_subsum
+
+TEST(SubsequencesTest, maximalSubsum_WhenNegativeIsLessThanSubsum_ThenIncludesNegative)
 {
+    // given
     std::vector<double> sequence = {3.5, 4.8, -1.6, 7.7, 2.1, -9.3, 0.8};
-
+    // when
     double result = alse::maximal_subsum(sequence);
-
+    // then
     EXPECT_DOUBLE_EQ(16.5, result);
 }
 
-TEST(MaximumSubarrayTest, maximalSubsum2)
+TEST(SubsequencesTest, maximalSubsum_WhenNegativeIsGreaterThanSubsum_ThenExcludesNegative)
 {
+    // given
     std::vector<double> sequence = {-9.3, -1.2, 3.5, 4.8, -10.6, 7.7, 2.1, 0.8, 4.0};
-
+    // when
     double result = alse::maximal_subsum(sequence);
-
+    // then
     EXPECT_DOUBLE_EQ(14.6, result);
 }
 
-TEST(MaximumSubarrayTest, maximalSubsum_WhenAllElementsAreNegative)
+TEST(SubsequencesTest, maximalSubsum_WhenAllElementsAreNegative_ThenZero)
 {
+    // given
     std::vector<double> sequence = {-9.0, -2.4, -3.07, -1.93, -12.67};
-
+    // when
     double result = alse::maximal_subsum(sequence);
-
+    // then
     EXPECT_DOUBLE_EQ(0.0, result);
 }
+
+#pragma endregion
