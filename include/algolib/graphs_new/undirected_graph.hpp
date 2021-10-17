2b09010e6a9b3fb2133992df568e03882ba4a25d
R. Kaleta <rafkaleta@onet.pl>
2021-10-17 15:33:09 +0200

    Undirected graph

---
+++ include/algolib/graphs_new/undirected_graph.hpp
@@ -0,0 +1,180 @@
+/*!
+ * \file undirected_graph.hpp
+ * \brief Structure of undirected graph
+ */
+#ifndef UNDIRECTED_GRAPH_HPP_
+#define UNDIRECTED_GRAPH_HPP_
+
+#include <cstdlib>
+#include <algorithm>
+#include <exception>
+#include <stdexcept>
+#include <unordered_set>
+#include <vector>
+#include "directed_graph.hpp"
+#include "simple_graph.hpp"
+
+namespace algolib::graphs
+{
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    struct undirected_graph : public virtual graph<VertexId, VertexProperty, EdgeProperty>
+    {
+        using vertex_id_type =
+                typename graph<VertexId, VertexProperty, EdgeProperty>::vertex_id_type;
+        using vertex_type = typename graph<VertexId, VertexProperty, EdgeProperty>::vertex_type;
+        using edge_type = typename graph<VertexId, VertexProperty, EdgeProperty>::edge_type;
+        using vertex_property_type =
+                typename graph<VertexId, VertexProperty, EdgeProperty>::vertex_property_type;
+        using edge_property_type =
+                typename graph<VertexId, VertexProperty, EdgeProperty>::edge_property_type;
+
+        ~undirected_graph() override = default;
+    };
+
+    template <typename VertexId = size_t, typename VertexProperty = no_prop,
+              typename EdgeProperty = no_prop>
+    class undirected_simple_graph
+        : public simple_graph<VertexId, VertexProperty, EdgeProperty>,
+          public virtual undirected_graph<VertexId, VertexProperty, EdgeProperty>
+    {
+    public:
+        using vertex_id_type =
+                typename simple_graph<VertexId, VertexProperty, EdgeProperty>::vertex_id_type;
+        using vertex_type =
+                typename simple_graph<VertexId, VertexProperty, EdgeProperty>::vertex_type;
+        using edge_type = typename simple_graph<VertexId, VertexProperty, EdgeProperty>::edge_type;
+        using vertex_property_type =
+                typename simple_graph<VertexId, VertexProperty, EdgeProperty>::vertex_property_type;
+        using edge_property_type =
+                typename simple_graph<VertexId, VertexProperty, EdgeProperty>::edge_property_type;
+
+    protected:
+        using repr = typename simple_graph<VertexId, VertexProperty, EdgeProperty>::repr;
+
+    public:
+        explicit undirected_simple_graph(const std::vector<vertex_type> & vertices = {})
+            : simple_graph<VertexId, VertexProperty, EdgeProperty>(vertices)
+        {
+        }
+
+        ~undirected_simple_graph() override = default;
+        undirected_simple_graph(const undirected_simple_graph &) = default;
+        undirected_simple_graph(undirected_simple_graph &&) = default;
+        undirected_simple_graph & operator=(const undirected_simple_graph &) = default;
+        undirected_simple_graph & operator=(undirected_simple_graph &&) = default;
+
+        size_t output_degree(const vertex_type & vertex) const override
+        {
+            return this->representation.adjacent_edges(vertex).size();
+        }
+
+        size_t input_degree(const vertex_type & vertex) const override
+        {
+            return this->representation.adjacent_edges(vertex).size();
+        }
+
+        size_t edges_count() const override;
+        std::vector<edge_type> edges() const override;
+        edge_type add_edge(const edge_type & edge) override;
+        edge_type add_edge(const edge_type & edge, const edge_property_type & property) override;
+        directed_simple_graph<vertex_type, vertex_property_type, edge_property_type>
+                as_directed() const;
+    };
+
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    size_t undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::edges_count() const
+    {
+        auto edges_set = this->representation.edges_set();
+        std::unordered_set<edge_type> all_edges;
+
+        std::for_each(std::begin(edges_set), std::end(edges_set), [&](auto && edge_set) {
+            std::copy(std::begin(edge_set), std::end(edge_set), std::inserter(all_edges));
+        });
+
+        return all_edges.size();
+    }
+
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    std::vector<typename undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::edge_type>
+            undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::edges() const
+    {
+        auto && edges_set = this->representation.edges_set();
+        std::unordered_set<edge_type> all_edges;
+
+        std::for_each(std::begin(edges_set), std::end(edges_set), [&](auto && edge_set) {
+            std::copy(std::begin(edge_set), std::end(edge_set), std::inserter(all_edges));
+        });
+
+        return std::vector<edge_type>(all_edges.begin(), all_edges.end());
+    }
+
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    typename undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::edge_type
+            undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::add_edge(
+                    const typename undirected_simple_graph<VertexId, VertexProperty,
+                                                           EdgeProperty>::edge_type & edge)
+    try
+    {
+        this->get_edge(edge.source(), edge.destination());
+        throw std::invalid_argument("Edge already exists");
+    }
+    catch(const std::out_of_range &)
+    {
+        this->representation.add_edge_to_source(edge);
+        this->representation.add_edge_to_destination(edge);
+        return edge;
+    }
+
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    typename undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::edge_type
+            undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::add_edge(
+                    const typename undirected_simple_graph<VertexId, VertexProperty,
+                                                           EdgeProperty>::edge_type & edge,
+                    const typename undirected_simple_graph<
+                            VertexId, VertexProperty, EdgeProperty>::edge_property_type & property)
+    try
+    {
+        this->operator[](edge.source(), edge.destination());
+        throw std::invalid_argument("Edge already exists");
+    }
+    catch(const std::out_of_range &)
+    {
+        this->representation.add_edge_to_source(edge);
+        this->representation.add_edge_to_destination(edge);
+        this->representation.property(edge) = property;
+        return edge;
+    }
+
+    template <typename VertexId, typename VertexProperty, typename EdgeProperty>
+    directed_simple_graph<
+            typename undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::vertex_type,
+            typename undirected_simple_graph<VertexId, VertexProperty,
+                                             EdgeProperty>::vertex_property_type,
+            typename undirected_simple_graph<VertexId, VertexProperty,
+                                             EdgeProperty>::edge_property_type>
+            undirected_simple_graph<VertexId, VertexProperty, EdgeProperty>::as_directed() const
+    {
+        std::vector<vertex_type> all_vertices = this->vertices();
+        std::vector<vertex_id_type> vertex_ids;
+
+        std::transform(std::begin(all_vertices), std::end(all_vertices),
+                       std::back_inserter(vertex_ids),
+                       [](const vertex_type & vertex) { return vertex.id; });
+
+        directed_simple_graph<vertex_type, vertex_property_type, edge_property_type> graph(
+                vertex_ids);
+
+        for(auto && vertex : all_vertices)
+            graph.properties[vertex] = this->properties[vertex];
+
+        for(auto && edge : this->edges())
+        {
+            graph.add_edge(edge, this->properties[edge]);
+            graph.add_edge(edge.reversed(), this->properties[edge]);
+        }
+
+        return graph;
+    }
+}
+
+#endif
---
+++ src/algolib/graphs_new/undirected_graph.cpp
@@ -0,0 +1,5 @@
+/*!
+ * \file undirected_graph.cpp
+ * \brief Structure of undirected graph
+ */
+#include "algolib/graphs_new/undirected_graph.hpp"
