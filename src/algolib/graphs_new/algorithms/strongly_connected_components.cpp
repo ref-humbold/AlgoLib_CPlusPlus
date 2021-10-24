2b874b00182f77efb9af75290d258641e9f665d1
R. Kaleta <rafkaleta@onet.pl>
2021-10-24 09:00:21 +0200

    Topological sorting and SCC

---
+++ src/algolib/graphs_new/algorithms/strongly_connected_components.cpp
@@ -0,0 +1,5 @@
+/**!
+ * \file strongly_connected_components.cpp
+ * \brief Algorithm for strongly connected components
+ */
+#include "algolib/graphs_new/algorithms/strongly_connected_components.hpp"
---
+++ src/algolib/graphs_new/algorithms/topological_sorting.cpp
@@ -0,0 +1,5 @@
+/*!
+ * \file topological_sorting.cpp
+ * \brief Algorithms for topological sorting of a directed graph
+ */
+#include "algolib/graphs_new/algorithms/topological_sorting.hpp"
