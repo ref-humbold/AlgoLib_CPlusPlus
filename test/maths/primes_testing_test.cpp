735439f15eea2570efe7b8c53d646d0dc02399bb
R. Kaleta <rafkaleta@onet.pl>
2023-11-19 20:17:11 +0100

    Primes testing for long and long long

--- test/maths/primes_testing_test.cpp
+++ test/maths/primes_testing_test.cpp
@@ -41,19 +41,50 @@ TEST(PrimesTestingTest, testPrimeFermat_WhenPrime_ThenTrue)
     EXPECT_TRUE(result);
 }
 
-TEST(PrimesTestingTest, testPrimeFermat_WhenComposite1_ThenFalse)
+TEST(PrimesTestingTest, testPrimeFermat_WhenPrimeLong_ThenTrue)
 {
     // when
-    bool result = alma::test_prime_fermat(1001L);
+    bool result = alma::test_prime_fermat(2131L);
+    // then
+    EXPECT_TRUE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeFermat_WhenPrimeLongLong_ThenTrue)
+{
+    // when
+    bool result = alma::test_prime_fermat(6199LL);
+    // then
+    EXPECT_TRUE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeFermat_WhenComposite_ThenFalse)
+{
+    // when
+    bool result = alma::test_prime_fermat(1001L);  // 1001 = 7 * 11 * 13
     // then
     EXPECT_FALSE(result);
 }
 
-TEST(PrimesTestingTest, testPrimeFermat_WhenComposite2_ThenFalse)
+TEST(PrimesTestingTest, testPrimeFermat_WhenCompositeLong_ThenFalse)
 {
     // when
-    bool result =
-            alma::test_prime_fermat(41041LL);  // 41041 = 7 * 11 * 13 * 41 is a Carmichael number
+    bool result = alma::test_prime_fermat(41041L);  // 41041 = 7 * 11 * 13 * 41
+    // then
+    EXPECT_FALSE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeFermat_WhenCompositeLongLong_ThenFalse)
+{
+    // when
+    bool result = alma::test_prime_fermat(73627LL);  // 73627 = 17 * 61 * 71
+    // then
+    EXPECT_FALSE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeFermat_WhenCompositeSquareOfPrime_ThenFalse)
+{
+    // when
+    bool result = alma::test_prime_fermat(3481);  // 3481 = 59 ^ 2
     // then
     EXPECT_FALSE(result);
 }
@@ -93,7 +124,23 @@ TEST(PrimesTestingTest, testPrimeMiller_WhenPrime_ThenTrue)
     EXPECT_TRUE(result);
 }
 
-TEST(PrimesTestingTest, testPrimeMiller_WhenComposite1_ThenFalse)
+TEST(PrimesTestingTest, testPrimeMiller_WhenPrimeLong_ThenTrue)
+{
+    // when
+    bool result = alma::test_prime_miller(2131L);
+    // then
+    EXPECT_TRUE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeMiller_WhenPrimeLongLong_ThenTrue)
+{
+    // when
+    bool result = alma::test_prime_miller(6199LL);
+    // then
+    EXPECT_TRUE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeMiller_WhenComposite_ThenFalse)
 {
     // when
     bool result = alma::test_prime_miller(1001L);
@@ -101,10 +148,26 @@ TEST(PrimesTestingTest, testPrimeMiller_WhenComposite1_ThenFalse)
     EXPECT_FALSE(result);
 }
 
-TEST(PrimesTestingTest, testPrimeMiller_WhenComposite2_ThenFalse)
+TEST(PrimesTestingTest, testPrimeMiller_WhenCompositeLong_ThenFalse)
 {
     // when
-    bool result = alma::test_prime_miller(41041LL);
+    bool result = alma::test_prime_miller(41041L);
+    // then
+    EXPECT_FALSE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeMiller_WhenCompositeLongLong_ThenFalse)
+{
+    // when
+    bool result = alma::test_prime_miller(73627LL);
+    // then
+    EXPECT_FALSE(result);
+}
+
+TEST(PrimesTestingTest, testPrimeMiller_WhenCompositeSquareOfPrime_ThenFalse)
+{
+    // when
+    bool result = alma::test_prime_miller(3481);
     // then
     EXPECT_FALSE(result);
 }
